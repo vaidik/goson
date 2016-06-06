d892e993d5cb039766c7a01af5c5f6876a2e26c2
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2016-06-06 09:22:41 +0530

    run go fmt and use only camel case

--- goson.go
+++ goson.go
@@ -1,124 +1,124 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "os"
-    "reflect"
-    "strings"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"reflect"
+	"strings"
 
-    "github.com/Jeffail/gabs"
-    "github.com/codegangsta/cli"
+	"github.com/Jeffail/gabs"
+	"github.com/codegangsta/cli"
 )
 
 type loopTuple struct {
-    ForEach, AsItem string
+	ForEach, AsItem string
 }
 
 func zip(a, b []string) ([]loopTuple, error) {
-    if len(a) != len(b) {
-        return nil, fmt.Errorf("zip: arguments must be of the same length")
-    }
+	if len(a) != len(b) {
+		return nil, fmt.Errorf("zip: arguments must be of the same length")
+	}
 
-    r := make([]loopTuple, len(a))
-    for i, e := range a {
-        r[i] = loopTuple{e, b[i]}
-    }
+	r := make([]loopTuple, len(a))
+	for i, e := range a {
+		r[i] = loopTuple{e, b[i]}
+	}
 
-    return r, nil
+	return r, nil
 }
 
 func main() {
-    bytes, err := ioutil.ReadAll(os.Stdin)
-    if err != nil {
-        log.Fatal("Failed to read input from stdin.")
-    }
+	bytes, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal("Failed to read input from stdin.")
+	}
 
-    parsed_obj, err := gabs.ParseJSON(bytes)
-    if err != nil {
-        log.Fatal("Failed to parse input.")
-    }
+	parsedObj, err := gabs.ParseJSON(bytes)
+	if err != nil {
+		log.Fatal("Failed to parse input.")
+	}
 
-    app := cli.NewApp()
-    app.Name = "goson"
-    app.Usage = "Parse JSON on the CLI easily."
+	app := cli.NewApp()
+	app.Name = "goson"
+	app.Usage = "Parse JSON on the CLI easily."
 
-    asitem := make(cli.StringSlice, 0)
-    foreach := make(cli.StringSlice, 0)
-    app.Flags = []cli.Flag{
-        cli.StringSliceFlag{
-            Name:  "foreach",
-            Usage: "Loop over every item in a JSON body.",
-            Value: &foreach,
-        },
-        cli.StringSliceFlag{
-            Name:  "asitem",
-            Usage: "Assign every item looped over by foreach to a variable for future use.",
-            Value: &asitem,
-        },
-    }
+	asitem := make(cli.StringSlice, 0)
+	foreach := make(cli.StringSlice, 0)
+	app.Flags = []cli.Flag{
+		cli.StringSliceFlag{
+			Name:  "foreach",
+			Usage: "Loop over every item in a JSON body.",
+			Value: &foreach,
+		},
+		cli.StringSliceFlag{
+			Name:  "asitem",
+			Usage: "Assign every item looped over by foreach to a variable for future use.",
+			Value: &asitem,
+		},
+	}
 
-    app.Commands = []cli.Command{
-        {
-            Name:  "get",
-            Usage: "Get a key present in a JSON body.",
-            Action: func(c *cli.Context) {
-                foreachLoops := c.Parent().StringSlice("foreach")
-                asItems := c.Parent().StringSlice("asitem")
+	app.Commands = []cli.Command{
+		{
+			Name:  "get",
+			Usage: "Get a key present in a JSON body.",
+			Action: func(c *cli.Context) {
+				foreachLoops := c.Parent().StringSlice("foreach")
+				asItems := c.Parent().StringSlice("asitem")
 
-                loopTuples, err := zip(foreachLoops, asItems)
-                if err != nil {
-                    log.Fatal("Number of foreach(s) and asitems(s) should match.")
-                }
+				loopTuples, err := zip(foreachLoops, asItems)
+				if err != nil {
+					log.Fatal("Number of foreach(s) and asitems(s) should match.")
+				}
 
-                printableObj := runForEach(parsed_obj, loopTuples)
-                printableArr, _ := printableObj.Children()
+				printableObj := runForEach(parsedObj, loopTuples)
+				printableArr, _ := printableObj.Children()
 
-                for _, element := range printableArr {
-                    output := element.Path(c.Args().First()).Data()
-                    if strings.HasPrefix(reflect.TypeOf(output).String(), "map") {
-                        outputString, _ := json.Marshal(output)
-                        fmt.Println(string(outputString))
-                    } else {
-                        fmt.Println(output)
-                    }
-                }
-            },
-        },
-    }
+				for _, element := range printableArr {
+					output := element.Path(c.Args().First()).Data()
+					if strings.HasPrefix(reflect.TypeOf(output).String(), "map") {
+						outputString, _ := json.Marshal(output)
+						fmt.Println(string(outputString))
+					} else {
+						fmt.Println(output)
+					}
+				}
+			},
+		},
+	}
 
-    app.Run(os.Args)
+	app.Run(os.Args)
 }
 
 func runForEach(obj *gabs.Container, loops []loopTuple) *gabs.Container {
-    wrapped := gabs.New()
-    wrapped.Array("result")
+	wrapped := gabs.New()
+	wrapped.Array("result")
 
-    forEachObj := obj.Path(loops[0].ForEach)
+	forEachObj := obj.Path(loops[0].ForEach)
 
-    if len(loops) == 1 {
-        forEachArray, _ := forEachObj.Children()
+	if len(loops) == 1 {
+		forEachArray, _ := forEachObj.Children()
 
-        for _, element := range forEachArray {
-            elementObj := gabs.New()
-            elementObj.Set(element.Data(), loops[0].AsItem)
-            wrapped.ArrayAppend(elementObj.Data(), "result")
-        }
-    } else {
-        forEachArray, _ := forEachObj.Children()
-        for _, element := range forEachArray {
-            elementObj := gabs.New()
-            elementObj.Set(element.Data(), loops[0].AsItem)
+		for _, element := range forEachArray {
+			elementObj := gabs.New()
+			elementObj.Set(element.Data(), loops[0].AsItem)
+			wrapped.ArrayAppend(elementObj.Data(), "result")
+		}
+	} else {
+		forEachArray, _ := forEachObj.Children()
+		for _, element := range forEachArray {
+			elementObj := gabs.New()
+			elementObj.Set(element.Data(), loops[0].AsItem)
 
-            printableObj := runForEach(elementObj, loops[1:])
-            printableArr, _ := printableObj.Children()
-            for _, item := range printableArr {
-                wrapped.ArrayAppend(item.Data(), "result")
-            }
-        }
-    }
+			printableObj := runForEach(elementObj, loops[1:])
+			printableArr, _ := printableObj.Children()
+			for _, item := range printableArr {
+				wrapped.ArrayAppend(item.Data(), "result")
+			}
+		}
+	}
 
-    return wrapped.S("result")
+	return wrapped.S("result")
 }
